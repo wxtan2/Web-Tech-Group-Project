2fb1d349b26fc30328ea9df95c40c8369c8a68cb
wxtan2 <wxtan2@graduate.utm.my>
2022-04-22 03:12:00 +0800

    mobile menu

---
+++ js/js.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    var SITEURL = window.location.href.substring(0, window.location.href.lastIndexOf("/") + 1);
+    var path = window.location.pathname.split("/").pop();
+    $(".navBar").find('[data-menu="' + path + '"]').addClass("active");
+    if (path == "dashboard.jsp") {
+        $("#topMenuPage").addClass('court');
+    } else if (path == "staffBooking.jsp") {
+        $("#topMenuPage").addClass('booking');
+    } else if (path == "staffSetting.jsp") {
+        $("#topMenuPage").addClass('setting');
+    }
+
+    $("#mobileToggle").on("click", function() {
+        if ($(".navigation_bar").hasClass("active")) {
+            $(".navigation_bar").removeClass("active");
+            $(this).removeClass("active");
+
+
+        } else {
+            $(".navigation_bar").addClass("active");
+            $(this).addClass("active");
+
+        }
+
+    });
+
+
+})
