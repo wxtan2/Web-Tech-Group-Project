7b51b59aef8972e3805c8a4c81cd970675d9e936
wxtan2 <wxtan2@graduate.utm.my>
2022-04-24 22:56:06 +0800

    togglebutton

--- js/js.js
+++ js/js.js
@@ -1,22 +1,20 @@
 $(document).ready(function() {
-    var SITEURL = window.location.href.substring(0, window.location.href.lastIndexOf("/") + 1);
-    var path = window.location.pathname.split("/").pop();
-    $(".navBar").find('[data-menu="' + path + '"]').addClass("active");
-    if (path == "dashboard.jsp") {
-        $("#topMenuPage").addClass('court');
-    } else if (path == "staffBooking.jsp") {
-        $("#topMenuPage").addClass('booking');
-    } else if (path == "staffSetting.jsp") {
-        $("#topMenuPage").addClass('setting');
-    }
+    // var SITEURL = window.location.href.substring(0, window.location.href.lastIndexOf("/") + 1);
+    // var path = window.location.pathname.split("/").pop();
+    // $(".navBar").find('[data-menu="' + path + '"]').addClass("active");
+    // if (path == "dashboard.jsp") {
+    //     $("#topMenuPage").addClass('court');
+    // } else if (path == "staffBooking.jsp") {
+    //     $("#topMenuPage").addClass('booking');
+    // } else if (path == "staffSetting.jsp") {
+    //     $("#topMenuPage").addClass('setting');
+    // }
 
     $(".mobileToggle").on("click", function() {
         if ($(".navigation_bar").hasClass("active")) {
             $(".navigation_bar").removeClass("active");
             $(this).removeClass("active");
             $('.toggle').attr("name", "list")
-
-
 
         } else {
             $(".navigation_bar").addClass("active");
